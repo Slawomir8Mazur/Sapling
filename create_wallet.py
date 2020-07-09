495390ca633a48038ed13abec08a72ad6fffbc3f
qxz0qgq <3edc3edc>
2020-07-09 19:42:20 +0200

    update 1

--- create_wallet.py
+++ create_wallet.py
@@ -1,1 +1,19 @@
-# Source: https://raw.githubusercontent.com/zcash/zips/master/protocol/protocol.pdf
+# User has random value sk - spending key
+# https://zips.z.cash/zip-0032
+
+from hashlib import blake2b
+
+def BLAKE2b512(personalisation_string: bytes, data: bytes):
+    resp = blake2b(data=data, person=personalisation_string)
+    assert len(resp)==32 and isinstance(resp, bytes)
+    return resp
+
+
+S = bytes(b'   ')
+assert len(S)>=32
+
+I = BLAKE2b512(b'ZcashIPSapling', S)
+assert len(I) == 64
+I_l = I[:32]    # sk_m -master spending key
+I_r = I[-32:]   # c_m  -master chain code
+
---
+++ hash_functions.py
@@ -0,0 +1,4 @@
+from hashlib import blake2b
+
+def PRF_expand_sk():
+    pass
